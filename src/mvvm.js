0db1e25d1e3a21724acc5c6398de72643630f7a4
lulujianglab <18810330250@163.com>
2018-01-25 09:42:02 +0800

    update

--- src/mvvm.js
+++ src/mvvm.js
@@ -2,8 +2,8 @@ var element = document.getElementById('element')
 
 // 存放data数据
 var _model = {
-	name: 'zhangshan',
-	password: '1q2w3e4r'
+  name: 'zhangshan',
+  password: '1q2w3e4r'
 }
 
 var key = 'v-model'
@@ -11,46 +11,47 @@ var key = 'v-model'
 mvvm(element, _model)
 
 function mvvm(element, _model) {
-	var model = {}
+  var model = {}
 
-	for (let key in _model) {
-		// 使用Object.defineProperty定义新属性或修改原有的属性
-		Object.defineProperty(model, key, { // 将key属性转换为访问器属性，get/set用来更新和设置数据
+  for (let key in _model) {
+	// 使用Object.defineProperty定义新属性或修改原有的属性
+	Object.defineProperty(model, key, { // 将key属性转换为访问器属性，get/set用来更新和设置数据
+		
+	  get () {
+			return _model[key]
+	  },
 
-			get () {
-				return _model[key]
-			},
-
-			set (val) {
-				if (val === _model[key]) {
-					return
-				} else {
-					_model[key] = val
-					modelView()
-				}
-			}
-		})
-	}
-
-	modelView()
-
-	element.addEventListener('keyup', function (e) {
-		var el = e.target
-		if (el.getAttribute(key)) { // model目标对象，进行依赖收集
-			model[el.getAttribute(key)] = el.value
+	  set (val) {
+		if (val === _model[key]) {
+		  return
+		} else {
+		  _model[key] = val
+		  modelView()
 		}
+	  }
+	  
 	})
+  }
 
-	function modelView () {
-		var children = element.children
-		for (var i = 0; i < children.length; i++) {
-			var el = children[i]
+  modelView()
 
-			if (el.getAttribute(key)) {
-				el.value = model[el.getAttribute(key)]
-			}
-		}
+  element.addEventListener('keyup', function (e) {
+	var el = e.target
+	if (el.getAttribute(key)) { // model目标对象，进行依赖收集
+	  model[el.getAttribute(key)] = el.value
 	}
+  })
 
-	return model
+  function modelView () {
+	var children = element.children
+	for (var i = 0; i < children.length; i++) {
+	  var el = children[i]
+
+	  if (el.getAttribute(key)) {
+		el.value = model[el.getAttribute(key)]
+	  }
+	}
+  }
+
+  return model
 }
